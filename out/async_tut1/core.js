dfd780d69f498fd13fad43c459fdd8cddd81cfea
Ben Mathes <mathes.ben@gmail.com>
2013-11-09 14:08:40 +0800

    initial leiningen and async-tut1 committ

---
+++ out/async_tut1/core.js
@@ -0,0 +1,6 @@
+// Compiled by ClojureScript 0.0-2030
+goog.provide('async_tut1.core');
+goog.require('cljs.core');
+console.log("Hello world!");
+
+//# sourceMappingURL=core.js.map
---
+++ src/async_tut1/core.cljs
@@ -0,0 +1,3 @@
+(ns async-tut1.core)
+
+(. js/console (log "Hello world!"))
